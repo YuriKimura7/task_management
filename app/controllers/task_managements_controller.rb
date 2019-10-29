be545cff1ebbc19812db6db0a526206ee6662c96
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-29 10:09:34 +0900

    Closes #2 課題登録画面を作成する

--- app/controllers/task_managements_controller.rb
+++ app/controllers/task_managements_controller.rb
@@ -2,4 +2,22 @@ class TaskManagementsController < ApplicationController
   def index
     @task_managements = TaskManagement.all
   end
+
+  def new
+    @task_management = TaskManagement.new
+  end
+
+  def create
+    @task_management = TaskManagement.new(task_management_params)
+    if @task_management.save
+      redirect_to task_managements_path, notice: "課題を登録しました。"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def task_management_params
+    params.require(:task_management).permit(:title, :deadline, :memo)
+  end
 end
