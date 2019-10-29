253bacf4d7197de9c7dafdecf16a5e970c194ea4
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-29 11:16:23 +0900

    Closes #4 課題詳細画面を作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :task_managements, only: [:index, :new, :create]
+  resources :task_managements, except: [:edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
