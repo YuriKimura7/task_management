3ba385735cbae20a8e66ba703178993712eac993
Yuri Kimura <yuri.kimura@luxiar.com>
2019-10-30 16:07:51 +0900

    Closes #7 課題のステータスが変更できること

--- app/models/task_management.rb
+++ app/models/task_management.rb
@@ -2,6 +2,14 @@ class TaskManagement < ApplicationRecord
   validates :title, presence: true
   validates :deadline, presence: true
   validates :title, length: { maximum: 20 }
+  enum status:{ backlog: 0, wip: 1, closed: 2 }
+  validate :check_status
 
-  enum status:{ "未着手": 0, "着手": 1, "完了": 2 }
+  def check_status
+    if status == "wip"
+      if TaskManagement.where(status: "wip").count >= 1
+        errors.add(:status, "は着手中です")
+      end
+    end
+  end
 end
